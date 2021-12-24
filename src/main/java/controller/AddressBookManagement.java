2c75a0c1d3f37495fa7c707cb3e9467ab954c243
Ashok Koda <a.koda59@gmail.com>
2021-12-24 14:26:18 +0530

    Added:Welcome to Address Book

---
+++ src/main/java/controller/AddressBookManagement.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class AddressBookManagement {
+
+	public static void main(String[] args) {
+		
+		System.out.println("------Welcome To Address Book--------");
+
+	}
+
+}
